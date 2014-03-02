c69f24860b119d3b6f9d6d594f10aefbf27f1a9d
Gustavo Faria <gusfaria11@gmail.com>
2014-03-02 18:21:10 -0500

    adjustments

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
 $( document ).ready(function() {
-	$('#btn_send').click(function() {
-		console.log( "Handler for .click() called." );
+	//define array
+	var arrNames = [];
+	
+	//define input value
+	$( "#input_name" )
+	  .keyup(function() {
+	  var value = $( this ).val();
+	}).keyup();
+
+	  //button pressed
+	$('#btn_send').click(function(event) {
+		event.preventDefault();
+		arrNames.push($('#input_name').val())
+		console.log(arrNames);
+
+		$('#input_name').val(''); //clear the input name 
 	});
 
 });
