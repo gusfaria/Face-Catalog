098cfc93c8e5484d62dcf4f5e23f04533e54cb4d
Gustavo Faria <gusfaria11@gmail.com>
2014-04-02 21:23:33 -0400

    serial port module install

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app  = express();
 var fs = require('fs');
-
+var SerialPort = require("serialport").SerialPort
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -50,6 +50,22 @@ app.post("/uploadImage", function(req, res, next){
   	fs.writeFile("public/loaded_imgs/test"+ random_id +".png", data.data, function(err) {
   		console.log("error", err);
   	});
+});
+
+//serial port
+var serialPort = new SerialPort("/dev/tty.usbmodemfa131", {
+  baudrate: 9600
+}, false); // this is the openImmediately flag [default is true]
+
+serialPort.open(function () {
+  console.log('open');
+  serialPort.on('data', function(data) {
+    console.log('data received: ' + data);
+  });
+  serialPort.write("ls\n", function(err, results) {
+    console.log('err ' + err);
+    console.log('results ' + results);
+  });
 });
 
 
